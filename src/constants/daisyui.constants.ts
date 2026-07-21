994e872c1e438002b0df657d2248b5d088bf3179
Roger Andrés Venegas Opazo <127449516+rogandven@users.noreply.github.com>
2026-07-21 09:20:21 -0400

    Added better coloring

--- src/constants/daisyui.constants.ts
+++ src/constants/daisyui.constants.ts
@@ -15,5 +15,5 @@ export const AGE_BADGE_NAME: string = "age-badge";
 export const DAISYUI_BUTTON_COLORS: string[] = ["btn-primary", "btn-secondary", "btn-accent", "btn-info", "btn-success", "btn-warning", "btn-error"];
 export const DAISYUI_TEXT_COLORS: string[] = ["text-primary", "text-secondary", "text-accent", "text-info", "text-success", "text-warning", "text-error"];
 export const DAISYUI_PROGRESS_COLORS: string[] = ["progress-primary", "progress-secondary", "progress-accent", "progress-info", "progress-success", "progress-warning", "progress-error"];
-
+export const DAISYUI_LINK_COLORS: string[] = ["link-primary", "link-secondary", "link-accent", "link-info", "link-success", "link-warning", "link-error"];
 export const DIVIDER: string = "⋅";
