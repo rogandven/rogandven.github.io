b25715aba64b88f37d5703e340e75f6c2bd3afd3
rogandven <roger.venegas2301@alumnos.ubiobio.cl>
2026-08-12 04:57:44 -0400

    WIP: Carroussel is done, just need to center everything

--- src/components/ui/carroussel/Carroussel.tsx
+++ src/components/ui/carroussel/Carroussel.tsx
@@ -5,6 +5,7 @@ import { useState } from 'react';
 import { mod } from '../../../utils/general.utils';
 import { MdSchool } from 'react-icons/md';
 import { FaLocationDot } from "react-icons/fa6";
+import ShowMoreButton from './ShowMoreButton';
 
 const HIDDEN_PAGINATION_NAME = "hidden_pagination";
 
@@ -13,48 +14,50 @@ function Carroussel() {
     const [index, setIndex] = useState(0);
 
     const element = document?.getElementById(EDUCATION_CONTAINER_ID);
-    element && (element.className = "size-0 hidden");
+    element && (element.className = "hidden");
 
     console.log(JSON.stringify(data[index]));
     return (
-        <div className="flex flex-col w-133 h-100 overflow-clip">
-            <div>
-                <div className="card bg-base-100 w-133 h-100 shadow-md ">
-                    <div className="card-body">
-                        <h2 className="card-title text-accent">
-                            <MdSchool />
-                            {data[index].companyName}
-                        </h2>
-                        
-                        <div className='flex flex-row justify-start text-start items-center align-middle text-info'>
-                            <span><FaLocationDot className='mr-1' /></span>
-                            <p>{data[index].location}</p>
+        <div className="w-screen">
+            <div className="flex flex-col w-133 h-100 overflow-clip mt-5 mb-5 max-w-full items-center content-center">
+                <div className='max-w-full items-center'>
+                    <div className="card bg-base-100 w-133 h-100 shadow-md max-w-11/12">
+                        <div className="card-body">
+                            <h2 className="card-title text-accent">
+                                <MdSchool />
+                                {data[index].companyName}
+                            </h2>
+                            <ShowMoreButton id={"education" + index}/>
+                            <div className='flex flex-row justify-start text-start items-center align-middle text-info'>
+                                <span><FaLocationDot className='mr-1' /></span>
+                                <p>{data[index].location}</p>
+                            </div>
                         </div>
+                        <figure>
+                            <img 
+                                width={640}
+                                height={480}
+                                src={(data[index].imageURL || DEFAULT_IMAGE_URL).src}
+                                className='object-cover h-full w-full overflow-clip'
+                                alt={(data[index].companyName || data[index].name)}
+                            /> 
+                        </figure>
                     </div>
-                    <figure>
-                        <img 
-                            width={640}
-                            height={480}
-                            src={(data[index].imageURL || DEFAULT_IMAGE_URL).src}
-                            className='object-cover h-full w-full overflow-clip'
-                            alt={(data[index].companyName || data[index].name)}
-                        /> 
-                    </figure>
-                </div>
-            </div>            
-            <div className="w-full self-center items-center flex justify-center justify-items-center absolute mt-90">
-                <div className="join h-10 overflow-clip shadow-md">
-                    <button 
-                        className="join-item btn"
-                        onClick={() => {setIndex(mod((index - 1), data.length))}}
-                    >«</button>
+                </div>            
+                <div className="w-full self-center items-center flex justify-center justify-items-center absolute mt-90">
+                    <div className="join h-10 overflow-clip shadow-md">
+                        <button 
+                            className="join-item btn"
+                            onClick={() => {setIndex(mod((index - 1), data.length))}}
+                        >«</button>
 
-                    <button className="join-item btn w-15 overflow-clip">{data[index].startDate.getFullYear()}</button>
+                        <button className="join-item btn w-15 overflow-clip">{data[index].startDate.getFullYear()}</button>
 
-                    <button 
-                        className="join-item btn"
-                        onClick={() => {setIndex(mod((index + 1), data.length))}}
-                    >»</button>
+                        <button 
+                            className="join-item btn"
+                            onClick={() => {setIndex(mod((index + 1), data.length))}}
+                        >»</button>
+                    </div>
                 </div>
             </div>
         </div>
